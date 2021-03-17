2cb71f42b45d652577fe134ecf63fabb8db078a0
Nithin Mahendran <nithinmahendren@gmail.com>
2021-03-17 18:55:36 +0530

    Sign Out Function Has been added

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,20 @@
 import 'package:brew_coffee/screens/authenticate/authenticate.dart';
-import 'package:flutter/material.dart';
 import 'package:brew_coffee/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:brew_coffee/models/user.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
     // return either home or authenticate
 
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -32,4 +32,12 @@ class AuthService {
   //register with email and pass
 
   // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }

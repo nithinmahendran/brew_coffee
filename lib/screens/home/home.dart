5ae12fbc2ae17e7af69557e78ff636ed32d41357
Nithin Mahendran <nithinmahendren@gmail.com>
2021-04-04 13:15:11 +0530

    Bottom Sheet added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,24 +1,48 @@
 import 'package:brew_coffee/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_coffee/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_coffee/screens/home/brew_list.dart';
+import 'package:brew_coffee/models/brew.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text("Brew Coffee"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: [
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logout'))
-        ],
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+               child: Text('Bottom Sheet'),
+            );
+          });
+    }
+
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text("Brew Coffee"),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: [
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('Logout')),
+              FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('Settings'),
+              onPressed: () => _showSettingsPanel(),
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
